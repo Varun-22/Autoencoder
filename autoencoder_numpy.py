0b1cc28da9970e8c058545aef89715b371ad013c
Varun <varun.cce16@sot.pdpu.ac.in>
2020-05-16 20:42:23 +0530

    removed that extra enter

--- autoencoder_numpy.py
+++ autoencoder_numpy.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1MidDREAt8ahXPFzE8AlAGA_nDN5uYUgx
 """
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
